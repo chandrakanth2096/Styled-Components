17416a1c096e5780b3706c79ca927cbe14c767b0
chandrakanth2096 <chandrakanth2096@gmail.com>
2022-09-21 16:22:18 +0000

    styled-components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Heading, Button} from './StyledComponents'
+import './App.css'
+
+const App = () => (
+  <>
+    <Heading>Hello World</Heading>
+    <Button type="button">Click</Button>
+    <Button type="button" outline>
+      Click
+    </Button>
+  </>
+)
+
+export default App
