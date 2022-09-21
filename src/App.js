3ed987b534d5f78150b9926afadae83c1f3ff3bf
chandrakanth2096 <chandrakanth2096@gmail.com>
2022-09-21 16:51:01 +0000

    styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import {Heading, Button} from './StyledComponents'
+import {
+  GlobalStyle,
+  Heading,
+  Button,
+  CustomButton,
+  OutlineButton,
+} from './StyledComponents'
 import './App.css'
 
 const App = () => (
   <>
+    <GlobalStyle />
     <Heading>Hello World</Heading>
     <Button type="button">Click</Button>
     <Button type="button" outline>
       Click
     </Button>
+    <CustomButton type="button">Click!</CustomButton>
+    <OutlineButton type="button">Click!</OutlineButton>
   </>
 )
 
--- src/StyledComponents.js
+++ src/StyledComponents.js
@@ -1,8 +1,19 @@
-import styled from 'styled-components'
+import styled, {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+ body {
+  margin: 100px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: lightblue;
+}
+`
 
 export const Heading = styled.h1`
   color: #0070c1;
-  font-family: 'Roboto';
 `
 export const Button = styled.button`
   padding: 10px;
@@ -12,4 +23,19 @@ export const Button = styled.button`
   border-radius: 4px;
   border: 2px solid #0070c1;
   background-color: ${props => (props.outline ? '#fff' : '#0070c1')};
+`
+export const CustomButton = styled.button`
+  padding: 10px;
+  margin-right: 20px;
+  font-size: 15px;
+  color: #ffffff;
+  border-radius: 4px;
+  border: 2px solid #0070c1;
+  background-color: #0070c1;
+`
+
+export const OutlineButton = styled(CustomButton)`
+  color: #0070c1;
+  background-color: #ffffff;
+  font-family: 'Open sans';
 `
